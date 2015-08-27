e329968b46747e44b376cab365f2a06fc3435856
Boyuan Yang <073plan@gmail.com>
2015-08-27 10:28:59 +0800

    finish part of smserver.py

---
+++ robot/complete/smglobal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+##########################################################
+
+##
+# Global variables
+#
+# To record current running status.
+
+## Robot status tuple
+ROBOT_STATUS_LIST=("STANDBY", "USERPLAY", "USERPLAY_TRANSACTION", "MUSIC_PLAYER",)
+
+## Robot status
+ROBOT_STATUS=None
+
+## Subprocess list
+ROBOT_SUBPROCESSES=[]
+
+## END OF GLOBAL VARIABLES
+##########################################################
+
+#  vim: set ts=8 sw=4 tw=0 et :
