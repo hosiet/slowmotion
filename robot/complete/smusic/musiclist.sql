60acaa03f253fa47165e99af63c323353061ae4b
Boyuan Yang <073plan@gmail.com>
2015-09-19 15:42:16 +0800

    add new music

--- robot/complete/smusic/musiclist.sql
+++ robot/complete/smusic/musiclist.sql
@@ -5,6 +5,7 @@ id INTEGER PRIMARY KEY,
 filename TEXT NOT NULL,
 havenote INTEGER NOT NULL);
 INSERT INTO "musicdata" VALUES(null,'年轻的战场.mp3',0);
-INSERT INTO "musicdata" VALUES(null,'光荣啊中国共青团.mp3',0);
+INSERT INTO "musicdata" VALUES(null,'光荣啊中国共青团(young).mp3',0);
+INSERT INTO "musicdata" VALUES(null,'光荣啊,中国共青团.mp3',0);
 INSERT INTO "musicdata" VALUES(null,'困兽之斗.mp3',0);
 COMMIT;
