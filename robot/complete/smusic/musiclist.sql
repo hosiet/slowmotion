33804d4378b5b7f6af09e79e6e735a0f461e707e
Boyuan Yang <073plan@gmail.com>
2015-09-19 17:16:52 +0800

    add ramps

--- robot/complete/smusic/musiclist.sql
+++ robot/complete/smusic/musiclist.sql
@@ -8,4 +8,8 @@ INSERT INTO "musicdata" VALUES(null,'年轻的战场.mp3',0);
 INSERT INTO "musicdata" VALUES(null,'光荣啊中国共青团(young).mp3',0);
 INSERT INTO "musicdata" VALUES(null,'光荣啊,中国共青团.mp3',0);
 INSERT INTO "musicdata" VALUES(null,'困兽之斗.mp3',0);
+INSERT INTO "musicdata" VALUES(null,'节奏1.mp3',0);
+INSERT INTO "musicdata" VALUES(null,'节奏2.mp3',0);
+INSERT INTO "musicdata" VALUES(null,'节奏3.mp3',0);
+INSERT INTO "musicdata" VALUES(null,'节奏4.mp3',0);
 COMMIT;
