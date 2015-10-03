44cde17b7440b756be959736af3168d60679d80b
Boyuan Yang <073plan@gmail.com>
2015-10-03 10:49:21 +0800

    Initially finish autoplay

--- robot/complete/autoplay_main.py
+++ robot/complete/autoplay_main.py
@@ -9,32 +9,50 @@ import smglobal
 from smglobal import DEBUG
 
 class StrikeThread(threading.Thread):
-    def __init__(self):
+    """
+    StrikeThread, for the strike of autoplay.
+
+    When init, should take one argument: the id of which we need to play.
+
+    NOTE: id start with 1.
+    """
+    def __init__(self, autoplay_id):
         super().__init__(self)
-        self.conn = sqlite3.connect("smusic/new.db")
+        self.conn = sqlite3.connect("smusic/musiclist.db")
+        self.autoplay_id = autoplay_id;
 
     def _strike(self):
+        """
+        Actually do the strike work.
+        """
         c = self.conn.cursor()
-        for x in c.execute("SELECT time, note FROM music;"):
+        found = False
+        for x in c.execute("SELECT time, note FROM music WHERE id=?;", (self.autoplay_id, )):
+            found = True
             break
+        if not found:
+            # Given number not found.
+            # TODO ERROR LOGGING
+            return
         t = x[0].split()
         w = x[1].split()
         self.conn.close()
         s = sched.scheduler(time.time, time.sleep)
-        count = -1
+        count = 0
         for i in t:
-            count += 1
             floatnumber = float(t[count])
             s.enter(floatnumber, 1, keyboardplay.kp_play_note_once, argument=(w[count], ))
+            count += 1
         s.run()
 
     def run(self):
         self._strike()
 
-def test():
-    thread1 = StrikeThread()
+
+def sm_autoplay_test():
+    thread1 = StrikeThread(1)
     thread1.start()
     thread1.join()
 
 if __name__ == '__main__':
-    test()
+    sm_autoplay_test()
--- robot/complete/smusic/musiclist.sql
+++ robot/complete/smusic/musiclist.sql
@@ -12,4 +12,10 @@ INSERT INTO "musicdata" VALUES(null,'节奏1.mp3',0);
 INSERT INTO "musicdata" VALUES(null,'节奏2.mp3',0);
 INSERT INTO "musicdata" VALUES(null,'节奏3.mp3',0);
 INSERT INTO "musicdata" VALUES(null,'节奏4.mp3',0);
+CREATE TABLE music (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+time TEXT,
+note TEXT);
+INSERT INTO "music" VALUES(null,'star','1 1.7 2.4 3.1 3.8 4.5 5.2 6.6 7.3 8 8.7 9.4 10.1 10.8 12.2 12.9 13.6 14.3 15 15.7 16.4 17.8 18.5 19.2 19.9 20.6 21.3 22 23.4 24.1 24.8 25.5 26.2 26.9 27.6 29 29.7 30.4 31.1 31.8 32.5 33.2','1 1 5 5 6 6 5 4 4 3 3 2 2 1 5 5 4 4 3 3 2 5 5 4 4 3 3 2 1 1 5 5 6 6 5 4 4 3 3 2 2 1');
 COMMIT;
